04ae42e00503eba7d3607b6feea70278221091bf
Motty174 <sahakyanonik174@gmail.com>
2021-01-10 07:00:26 +0400

    End day 10.01.2021

--- public/scripts/header_search.js
+++ public/scripts/header_search.js
@@ -1,12 +1,12 @@
-const arr=document.getElementsByClassName('header_search')
+const arr=document.querySelectorAll('input')
+
 console.log(arr)
-arr[0].oninput=(eve)=>{
-    alert(1)
-}
-document.getElementById('header_search').oninput=(e) => {
+arr.forEach(elem => {
+
+elem.addEventListener('input', (e) => {
     e.preventDefault()
     const search_value = {
-        name: document.getElementById('header_search').value
+        name: arr[0].value || arr[1].value
     }
     fetch(`${local_host}searchForUser`,{
         method: "POST",
@@ -17,29 +17,58 @@ document.getElementById('header_search').oninput=(e) => {
     })
     .then(res => res.json() )
     .then(data => {
+        console.log(data)
         setTimeout(()=>{
+                if(screen.width>800){
+
+                    document.getElementById('found_users').innerHTML=""
        
-            document.getElementById('found_users').innerHTML=""
+                    if(data.array.length>=5){
+                      
+                        const found_user=document.createElement('li')
+                        found_user.innerHTML= "<a href='/users' > See all results </a>"
+                        found_user.classList.add('list-group-item')
+                        document.getElementById('found_users').append(found_user)
+                 
+                    }else{
+                 
+                    data.array.forEach((element) => {
+                        const found_user=document.createElement('li')
+                        found_user.innerText=element.name
+                        found_user.setAttribute('id',element._id)
+                        found_user.setAttribute('style','cursor: pointer;')
+                        found_user.classList.add('list-group-item')
+                        found_user.addEventListener('click',(event)=>{
+                            location.replace(`/users/${element._id}`)
+                        })
+                        document.getElementById('found_users').append(found_user)
+                    })}
+                }else{
+
+                    document.getElementById('found_users_mobile').innerHTML=''
        
-            if(data.array.length>=5){
-              
-                const found_user=document.createElement('li')
-                found_user.innerHTML= "<a href='/users' > See all results </a>"
-                found_user.classList.add('list-group-item')
-                document.getElementById('found_users').append(found_user)
-         
-            }else{
-         
-            data.array.forEach((element) => {
-                const found_user=document.createElement('li')
-                found_user.innerText=element.name
-                found_user.setAttribute('id',element._id)
-                found_user.setAttribute('style','cursor: pointer;')
-                found_user.classList.add('list-group-item')
-                found_user.addEventListener('click',(event)=>{
-                    location.replace(`/users/${element._id}`)
-                })
-                document.getElementById('found_users').append(found_user)
-            })}
+                    if(data.array.length>=5){
+                      
+                        const found_user=document.createElement('li')
+                        found_user.innerHTML= "<a href='/users' > See all results </a>"
+                        found_user.classList.add('list-group-item')
+                        document.getElementById('found_users_mobile').append(found_user)
+                 
+                    }else{
+                 
+                    data.array.forEach((element) => {
+                        const found_user=document.createElement('li')
+                        found_user.innerText=element.name
+                        found_user.setAttribute('id',element._id)
+                        found_user.setAttribute('style','cursor: pointer;')
+                        found_user.classList.add('list-group-item')
+                        found_user.addEventListener('click',(event)=>{
+                            location.replace(`/users/${element._id}`)
+                        })
+                        document.getElementById('found_users_mobile').append(found_user)
+                    })}
+                }
+
         },400)})
-}
+})
+})
